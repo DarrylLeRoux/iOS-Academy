207a5fdbe425d161a9e2446098ebba61ab5f3c62
Darryl Le Roux <devdarryl@outlook.com>
2023-01-17 11:42:39 +0200

    Initial Commit

---
+++ Emoji Picker/Emoji_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_PickerApp.swift
+//  Emoji Picker
+//
+//  Created by Darryl Le Roux on 2023/01/17.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
